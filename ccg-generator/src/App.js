462560b38ce4eadc0e2e15bf061fd0c7524e17b4
Greg Schuman <gcschuman@hotmail.com>
2020-12-03 20:34:44 -0800

    Working on the structure of how to get the cards to generate in app.js.

--- ccg-generator/src/App.js
+++ ccg-generator/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import QuoteAndAuthor from './components/CardGenerator';
+import Yellow from './YellowDB';
+import White  from './WhiteDB';
+export default class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //state
+  state = {
+    cards: cards[0].cards,
+    yellow: Yellow[0].yellow,
+    white: White[0].white
+  }
+
+  //generate diffrent quote function
+  generateRandomCard = (arr) => {
+    //get random numbers
+
+    let num = Math.floor(Math.random() * cards.length)
+
+    let newCard = cards[num];
+
+    //update state
+    this.setState({
+      quote: newCard.quote,
+      author: newCard.author
+    })
+
+    this.shuffleCards(cards)
+
+  }
+
+  //shuufle quotes function
+  shuffleCards = (arr) => {
+    return arr.sort(function () { return 0.5 - Math.random() });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="text-center">Catholic Card Game Generator</h1>
+        <QuoteAndAuthor
+          generateRandomQuote={this.generateRandomQuote}
+          quote={this.state}
+        />
+      </div>
+    )
+  }
 }
 
-export default App;
